1dbfd87bfe72841b9f250e7fedb5613986235550
César Pereira <Csar Pereira>
2016-09-15 15:59:31 -0300

    Commit for the weeks 3 and 4

---
+++ cesarapp/public/javascripts/startTest.js
@@ -0,0 +1,52 @@
+var startTime = 0;
+var waitInterval = 0;
+
+function onLoad() {
+  document.getElementById("divcircle").style.display = "none";
+  document.getElementById("endButton").style.display = "none";
+  $("#startButton").focus();
+}
+
+function startTest() {
+  document.getElementById("startButton").style.display = "none";
+  document.getElementById("endButton").style.display = "inline";
+  $("#endButton").focus();
+  document.getElementById("divcircle").style.display = "inline";
+  $("#desc").fadeOut(2000);
+  $("#divcircle")
+    .css({
+      opacity: 0,
+      left: "500px",
+    })
+    .animate({
+      opacity: 1,
+      left: 0,
+    }, 1500);
+
+  waitInterval = (Math.random() * 5) + 2;
+  startTime = new Date().getTime();
+
+  sleep(waitInterval * 1000).then(() => {
+    document.getElementById("circle").style.fill = "blue";
+  });
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function endTest() {
+  var endTime = new Date().getTime();
+
+  if (endTime - startTime < waitInterval * 1000) {
+    window.alert("Clicked before the circle became blue :(");
+  } else {
+    window.alert("Reaction time: " + (endTime - (startTime + waitInterval * 1000)) + " ms");
+  }
+
+  document.getElementById("startButton").style.display = "inline";
+  $("#startButton").text("Retry");
+  $("#startButton").focus();
+  document.getElementById("endButton").style.display = "none";
+  document.getElementById("circle").style.fill = "red";
+}
--- cesarapp/routes/index.js
+++ cesarapp/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({ extended: false });
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
 });
+
+/* POST method to save names given as input. */
+router.post('/process_post', urlEncodedParser, function (req, res) {  
+  router.use(bodyParser.json());
+  
+  var fileString = fs.readFileSync('public/names.json');  // String from the file content
+
+  var fileJSON = JSON.parse(fileString);
+  fileJSON.push({
+    first_name:req.body.first_name,
+    last_name:req.body.last_name 
+  });
+
+  fs.writeFile('public/names.json', JSON.stringify(fileJSON), (err) => {
+    res.send('Your name was successfully registered!')
+  })
+})
+
+/* GET method to list names stored in the text file. */
+router.get('/process_get', function (req, res) {  
+  router.use(bodyParser.json());
+  
+  var fileString = fs.readFileSync('public/names.json');  // String from the file content
+
+  var fileJSON = JSON.parse(fileString);
+
+  var textToSend = "";
+
+  // Iterating through the JSON array and concatening the names
+  for(var i = 0; i < fileJSON.length; i++) {
+    textToSend += fileJSON[i].first_name + " " + fileJSON[i].last_name + "<br>";
+  }
+
+  res.send(textToSend);
+})
 
 module.exports = router;
